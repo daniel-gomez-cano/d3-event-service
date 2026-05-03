08c13ccc1981b561bc4f1761d66b0370a361efad
F4RGAN2007 <avalo.santiago@correounivalle.edu.co>
2026-05-03 14:31:47 -0500

    Estructura base

---
+++ src/main/java/co/empresa/proyecto_desarrollo3/ProyectoDesarrollo3Application.java
@@ -0,0 +1,13 @@
+package co.empresa.proyecto_desarrollo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDesarrollo3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoDesarrollo3Application.class, args);
+    }
+
+}
---
+++ src/test/java/co/empresa/proyecto_desarrollo3/ProyectoDesarrollo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.empresa.proyecto_desarrollo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDesarrollo3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
